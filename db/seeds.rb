fa1449910abfdb1022e8a6a992ee90ca0e717d76
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2016-10-08 08:28:01 +0200

    Added admin flag to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+unless User.exists?(email: "admin@cfp.smartgioiosa.it")
+  User.create!(email: "admin@cfp.smartgioiosa.it", password: "password", admin: true
+)
+end
