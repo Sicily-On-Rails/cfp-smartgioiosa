ad215228a2e61f813d1ac44d7b75286d6246818f
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2016-10-08 06:26:50 +0200

    Updated devise views

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match 'contatti' => 'contacts#new', via: :get
   resources :contacts, path:'contatti', only: [:new, :create]
+  namespace :users do
+    resources :proposals
+  end
+
 end
