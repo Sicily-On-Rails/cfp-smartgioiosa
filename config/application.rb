6e5ba2cc2b722bb4b4d5140f824f70b74635ec16
ALESSANDRO <alexscaffidi@hotmail.it>
2016-10-08 10:47:28 +0200

    added locales it

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ Bundler.require(*Rails.groups)
 
 module CfpSmartgioiosa
   class Application < Rails::Application
+
+    config.time_zone = 'Rome'
+    I18n.available_locales = [:it, :en]
+    config.i18n.default_locale = :it
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded
